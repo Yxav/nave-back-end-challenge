a21b4a0e5d0196d5f320feacd1146caf2c5d007b
Yuri Xavier <yuri_dsx_@hotmail.com>
2020-09-16 19:57:44 -0300

    Fix model file and using ORM

--- controllers/naver-controller.js
+++ controllers/naver-controller.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken')
 const auth = require("../middlewares/authenticate")
 const validator = require("../bin/helpers/validator-service")
 const knex = require('knex')
-const naverModel = require("../models/Naver")
-const projectModel = require("../models/Project")
+const Naver = require('../models/Naver')
 
 exports.store = async(req, res) => {
 
@@ -24,23 +23,16 @@ exports.store = async(req, res) => {
     }
 
     try {
-        const naver = await naverModel.createNaver({
-            name,
-            birthdate,
-            admission_date,
-            job_role,
-            id_admin
-        })
-
-        if (projects) {
-            const id_naver = parseInt(naver)
-            for (var index_projects = 0; index_projects < projects.length; index_projects++) {
-                await naverModel.createProjectNaver({
-                    id_naver,
-                    id_project: projects[index_projects]
+        const naver = await Naver
+                .query()
+                .insert({
+                        name,
+                        birthdate,
+                        admission_date,
+                        job_role,
+                        id_admin
                 })
-            }
-        }
+
         res.send(req.body).status(200);
     } catch (e) {
         res.status(500).send({ message: "Internal error", error: e });
@@ -66,7 +58,10 @@ exports.index = async(req, res) => {
     }
 
     try {
-        const navers = await naverModel.index(query)
+        const navers = await Naver
+                .query()
+                .select('id', 'name', 'birthdate', 'admission_date', 'job_role')
+                .where(query)
 
         if (navers.length == 0) {
             res.status(404).send({ message: "There aren't navers" })
@@ -85,26 +80,20 @@ exports.show = async(req, res) => {
     const id_admin = req.loggedUser.user.id;
 
     try {
-        const naver = await naverModel.show({
-            'id': id,
-            'id_admin': id_admin
-        })
+        const naver = await Naver
+                .query()
+                .select('id', 'name', 'birthdate', 'admission_date', 'job_role')
+                .where({
+                    'id': id,
+                    'id_admin': id_admin
+                })
+                .first()
 
         if (!naver) {
             res.status(404).send({ message: "Naver not found" })
             return
         }
 
-        const project_id = await naverModel.showProject({ 'id_naver': id })
-
-
-        naver.projects = []
-
-        for (var index_id = 0; index_id < project_id.length; index_id++) {
-
-            naver.projects.push(await projectModel.show({ id: project_id[index_id].id_project }))
-
-        }
 
         res.send(naver).status(200)
 
@@ -140,45 +129,18 @@ exports.update = async(req, res) => {
                 birthdate,
                 admission_date,
                 job_role,
-                id_admin
             },
             id: {
-                'id': id
+                'id': id,
+                'id_admin': id_admin
             }
         }
-        const naver = await naverModel.update(data)
+        const naver = await Naver
+                    .query()
+                    .where(data.id)
+                    .patch(data.content)
 
-        const id_naver = parseInt(naver)
-
-        const arrProject = await naverModel.showProject({ id_naver: id })
-
-        if (projects.length == arrProject.length) {
-            for (var index_projects = 0; index_projects < projects.length; index_projects++) {
-                await naverModel.updateProject({
-                    context: {
-                        id_project: projects[index_projects]
-                    },
-                    condition: {
-                        'id_naver': id_naver
-                    }
-                })
-            }
-        }
-
-        await naverModel.deleteNaversProject({ id_naver: id })
-
-        for (var index_projects = 0; index_projects < projects.length; index_projects++) {
-
-            await naverModel.createProjectNaver({
-                id_naver,
-                id_project: projects[index_projects]
-            })
-        }
-
-        if (naver.length == 0) {
-            throw error;
-        }
-
+    
         res.status(200).send(req.body);
     } catch (e) {
         res.status(500).send({ message: "Internal error", error: e });
@@ -190,10 +152,13 @@ exports.delete = async(req, res) => {
     const id_admin = req.loggedUser.user.id;
 
     try {
-        await naverModel.delete({
-            'id': id,
-            'id_admin': id_admin
-        })
+        await Naver
+            .query()
+            .delete()
+            .where({
+                'id': id,
+                'id_admin': id_admin
+            })
 
         res.send({ message: "Deleted" }).status(200)
 
--- models/Naver.js
+++ models/Naver.js
@@ -1,58 +1,82 @@
 const db = require('../database/db')
 
 
-exports.createNaver = async(data) => {
-    return await db('navers').insert(data, 'id')
+const { Model } = require('objection')
+Model.knex(db)
+
+
+class Naver extends Model {
+    static get tableName(){
+        return 'navers'
+    }
 }
 
-exports.createProjectNaver = async(data) => {
-    await db('project_navers').insert(data)
+module.exports = Naver
 
-}
 
-exports.index = async(data) => {
-    return await db('navers')
-        .select('id', 'name', 'birthdate', 'admission_date', 'job_role')
-        .where(data);
-}
+// change all the models
+// later validation with Joi
 
-exports.show = async(data) => {
-    return await db('navers')
-        .select('id', 'name', 'birthdate', 'admission_date', 'job_role')
-        .where(data)
-        .first()
-}
 
-exports.showProject = async(data) => {
-    return await db('project_navers')
-        .select('id_project')
-        .where(data)
+// exports.createNaver = async(data) => {
+//     return await db('navers').insert(data, 'id')
+// }
 
-}
+// exports.createProjectNaver = async(data) => {
+//     await db('project_navers').insert(data)
 
-exports.update = async(data) => {
-    return await db('navers')
-        .where(data.id)
-        .update(data.content, 'id')
-}
+// }
 
-exports.updateProject = async(data) => {
-    await db('project_navers')
-        .where(data.condition)
-        .update(data.context)
+// exports.index = async(data) => {
+//     return await db('navers')
+//         .select('id', 'name', 'birthdate', 'admission_date', 'job_role')
+//         .where(data);
+// }
 
-}
+// exports.show = async(data) => {
+//     return await db('navers')
+//         .select('id', 'name', 'birthdate', 'admission_date', 'job_role')
+//         .where(data)
+//         .first()
+// }
 
-exports.delete = async(data) => {
-    await db('navers')
-        .where(data)
-        .first()
-        .delete()
-}
+// exports.showProject = async(data) => {
+//     return await db('project_navers')
+//         .select('id_project')
+//         .where(data)
 
-exports.deleteNaversProject = async(data) => {
-    await db('project_navers')
-        .where(data)
-        .first()
-        .delete()
-}
+// }
+
+// exports.update = async(data) => {
+//     return await db('navers')
+//         .where(data.id)
+//         .update(data.content, 'id')
+// }
+
+// exports.updateProject = async(data) => {
+//     await db('project_navers')
+//         .where(data.condition)
+//         .update(data.context)
+
+// }
+
+// exports.delete = async(data) => {
+//     await db('navers')
+//         .where(data)
+//         .first()
+//         .delete()
+// }
+
+// exports.deleteNaversProject = async(data) => {
+//     await db('project_navers')
+//         .where(data)
+//         .first()
+//         .delete()
+// }
+
+
+
+
+// Change to model 
+//add prettier and linter
+//add validation service and middleware
